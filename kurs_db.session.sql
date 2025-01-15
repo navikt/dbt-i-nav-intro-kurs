8e941494776a10766de9192504c4d9c94461e822
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2025-01-15 12:34:30 +0000

    legger inn data - endrer til ny extension for dbduck(unngå lock)

---
+++ kurs_db.session.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE orders AS SELECT * FROM read_csv_auto('/workspaces/dbt-i-nav-intro-kurs/intro_kurs/data/orders.csv');
+
+CREATE TABLE payments AS SELECT * FROM read_csv_auto('/workspaces/dbt-i-nav-intro-kurs/intro_kurs/data/payments.csv');
+
+CREATE TABLE customers AS SELECT * FROM read_csv_auto('/workspaces/dbt-i-nav-intro-kurs/intro_kurs/data/customers.csv');
+
+commit;
