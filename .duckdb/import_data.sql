daad6845db6e6829e4efdc2ce39a3eddde2fb103
Patrick Pedersen <patrick.pedersen@nav.no>
2025-01-31 18:27:55 +0100

    Legge til extension dbcode

--- .duckdb/import_data.sql
+++ .duckdb/import_data.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS raw;
-
 CREATE OR REPLACE TABLE orders AS SELECT * FROM read_csv_auto('.duckdb/data/orders.csv');
 
 CREATE OR REPLACE TABLE payments AS SELECT * FROM read_csv_auto('.duckdb//data/payments.csv');
