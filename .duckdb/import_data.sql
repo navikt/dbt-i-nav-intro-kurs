80013f5f6c6e114cb49d755388a98a6d12d21c04
Patrick Pedersen <patrick.pedersen@nav.no>
2025-02-07 15:26:57 +0100

    Legge rå-data i schema som gjenspeiler kurset
    Vi hadde problemer med at rådata lå i samme schema som dbt skulle bruke
    fordi mange kursdeltakere kalte modeller det samme som rådata-tabellene
    og derfor overskrev de.
    
    ref: https://nav-it.slack.com/archives/C07UR6D1C1G/p1738845056199489?thread_ts=1738824543.318809&cid=C07UR6D1C1G

--- .duckdb/import_data.sql
+++ .duckdb/import_data.sql
@@ -1,5 +1,6 @@
-CREATE OR REPLACE TABLE orders AS SELECT * FROM read_csv_auto('.duckdb/data/orders.csv');
+CREATE SCHEMA IF NOT EXISTS jaffle_shop;
+CREATE OR REPLACE TABLE jaffle_shop.orders AS SELECT * FROM read_csv_auto('.duckdb/data/orders.csv');
+CREATE OR REPLACE TABLE jaffle_shop.customers AS SELECT * FROM read_csv_auto('.duckdb/data/customers.csv');
 
-CREATE OR REPLACE TABLE payments AS SELECT * FROM read_csv_auto('.duckdb//data/payments.csv');
-
-CREATE OR REPLACE TABLE customers AS SELECT * FROM read_csv_auto('.duckdb/data/customers.csv');
+CREATE SCHEMA IF NOT EXISTS stripe;
+CREATE OR REPLACE TABLE stripe.payments AS SELECT * FROM read_csv_auto('.duckdb//data/payments.csv');
